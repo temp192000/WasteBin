00cb0a7d443d5a04f78861066e20f86332885493
temp192000 <codemonkey.ranjith@gmail.com>
2022-02-18 23:30:23 +0530

    feat: Menu Buttons; SSR Pages: New, default

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
-
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('code-display');
+    const code = `Welcome to WasteBin!\n\nUse the commands in the top right corner to create a new file and share with others.`
+    res.render('code-display', {code});
 });
 
+app.get('/new', (req, res) => {
+    res.render('new');
+})
+
 app.listen('4321', () => {
-    console.log("Server Started on http://localhost:4321")
+    console.log("Server Started on http://localhost:4321");
 })
